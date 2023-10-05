dda3789761a251783d43f9d91bbf8bc647702cf3
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-06 05:22:26 +0530

    Sign Up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/users')
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -20,9 +22,27 @@ module.exports.signIn = function(req, res){
     })
 }
 
+
 // get the sign up data
-module.exports.create = function(req, res){
-    // TODO later
+module.exports.create = async function(req, res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    try {
+        const user = await User.findOne({email: req.body.email});
+
+        if (!user){
+            await User.create(req.body);
+            return res.redirect('/users/sign-in');
+        }else{
+            return res.redirect('back');
+        }
+        
+    } catch (err) {
+        console.log('error in finding or creating user while signing up');
+        return;
+    }
 }
 
 
