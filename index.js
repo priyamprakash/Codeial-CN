222ceb531ff29ffb08af0d056421315e1a7b6b93
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-05 05:38:14 +0530

    Added Header and Footer

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 //Use express route 
 app.use('/', require('./routes'));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 app.listen(port, function(err){
 
     if(err){
