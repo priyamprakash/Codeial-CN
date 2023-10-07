badf061f0e69d69e31df930d7911524bca4b7fbe
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-08 03:42:09 +0530

    Added session-cookie code with latest updated code

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts')
 const db = require('./config/mongoose')
 // used for session cookie
-const session = require('express-session')
-const passport = require('passport')
-const passportLocal = require('./config/passport-local-strategy')
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -22,16 +23,20 @@ app.set('layout extractScripts',true)
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+//mongo store is used to store the session cookie in DB 
 app.use(session({
     name: 'codeial',
-    //change the secret before deployment in production
     secret: 'blahsomething',
     saveUninitialized: false,
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
-}))
+    },
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://127.0.0.1/codial_developemnt',
+        collectionName: 'sessions'
+    })
+}));
 
 app.use(passport.initialize())
 app.use(passport.session())
