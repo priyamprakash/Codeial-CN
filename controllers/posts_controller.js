374409cf6694ec447414cef88b6e9c50d7da00fd
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-11 04:04:05 +0530

    delete  posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post')
+const Comment = require('../models/comment')
 
 module.exports.create = function(req, res){
     Post.create({
@@ -17,3 +18,24 @@ module.exports.create = function(req, res){
         return res.status(500).send({message: 'Something went wrong'});
     });
 }
+
+
+module.exports.destroy = async function(req, res){
+    try {
+        let post = await Post.findById(req.params.id);
+
+        if (post.user == req.user.id){
+            await Post.deleteOne({_id: req.params.id});
+
+            await Comment.deleteMany({post: req.params.id});
+
+            return res.redirect('back');
+        } else {
+            return res.redirect('back');
+        }
+    } catch(err) {
+        console.error(err);
+        // handle error
+    }
+}
+
