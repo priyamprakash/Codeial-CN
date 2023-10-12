169e054e78c309bd15274d11f98ca87ff4d3f701
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-13 02:52:05 +0530

    Delete ur comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,27 @@ module.exports.create = async function(req, res){
         // handle error
     }
 }
+
+
+module.exports.destroy = async function(req, res){
+    try {
+        let comment = await Comment.findById(req.params.id);
+
+        if (comment.user == req.user.id){
+            let postId = comment.post;
+
+            await Comment.deleteOne({_id: req.params.id});
+
+            await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            return res.redirect('back');
+        } else {
+            return res.redirect('back');
+        }
+    } catch(err) {
+        console.error(err);
+        // handle error
+    }
+}
+
+
