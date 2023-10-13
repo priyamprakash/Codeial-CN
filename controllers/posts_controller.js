91795788527483a2f045cdc8d768fbf4b1f81d9f
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-13 06:02:31 +0530

    Include AJAX and update profile route update with POST Method

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,12 +3,22 @@ const Comment = require('../models/comment')
 
 module.exports.create = async function(req, res){
     try {
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
-        req.flash('success', 'Post Published');
+        
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
 
+        req.flash('success', 'Post published!');
+        
         return res.redirect('back');
     } catch(err) {
         req.flash('error', err);
